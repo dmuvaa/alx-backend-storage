2b9d2b30314cef13e8515cdfaad0ad34d49084c8
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-19 10:45:02 +0000

    Average Score Calculation

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,23 @@
--- script that creates a stored procedure
-DELIMITER //
+- script that creates a stored procedure
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-	SELECT AVG(score) INTO avg_score
-	FROM corrections
-	WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-	UPDATE users
-	SET average_score = avg_score
-	WHERE id = user_id;
-END //
-
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
