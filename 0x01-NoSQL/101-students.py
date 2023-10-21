7c7848d1cbfb74a4e4a5423cf460f290ead2f16e
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-22 02:48:50 +0300

    function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,14 @@
+#!/usr/bin/emv python3
+
+"""create a function"""
+
+
+def top_students(mongo_collection):
+    """function that returns all students sorted by average score"""
+    pipeline = [{"$project": {"_id": 1, "name": 1,
+                            "averageScore": {
+                                "$avg": {
+                                    "$avg": "$topics.score", },
+                                }, "topics": 1, }, },
+                {"$sort": {"averageScore": -1}, }, ]
+    return list(mongo_collection.aggregate(pipeline))
