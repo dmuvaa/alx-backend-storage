a7d02d23e2f2d46ee78aeb56b804d39b1814e3be
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-25 00:04:42 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,7 +15,8 @@ def count_calls(method: Callable) -> Callable:
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs) -> Any:
         """function that increments the count in Redis for the given method"""
-        self._redis.incr(method.__qualname__)
+        key = method.__qualname__
+        self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
 
@@ -30,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """method that creates a store"""
         key = str(uuid4())
