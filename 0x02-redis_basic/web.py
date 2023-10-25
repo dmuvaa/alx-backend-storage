1e442f1b5ddd99371e72ef7ea225c3fc5ce20811
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-25 13:27:08 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,32 +6,34 @@ import requests
 from cachetools import cached, TTLCache
 from functools import wraps
 
-cache = TTLCache(maxsize=100, TTL=10)
-
-url_count = {}
-
 """create a python class"""
 
 
-def count_access(func):
-    """tracks the number of times the url is accessed"""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        """function takes url as its first argument"""
-        url = args[0]
-        if url in url_count:
-            url_count[url] += 1
-        else:
-            url_count[url] = 1
-        print(f"URL '{url}' has been accessed {url_count[url]} times.")
-        return func(*args, **kwargs)
-    return wrapper
+class WebPageFetcher:
+    """A class to fetch web pages with caching and access counting."""
 
+    def __init__(self, maxsize=100, ttl=10):
+        self.cache = TTLCache(maxsize=maxsize, ttl=ttl)
+        self.url_count = {}
 
-@count_access
-@cached(cache)
-def get_page(url: str) -> str:
-    """function uses the requests module to get the HTML content of a URL"""
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
+    def count_access(func):
+        """tracks the number of times the url is accessed"""
+        @wraps(func)
+        def wrapper(instance, *args, **kwargs):
+            """function takes url as its first argument"""
+            url = args[0]
+            if url in instance.url_count:
+                instance.url_count[url] += 1
+            else:
+                instance.url_count[url] = 1
+            print(f"URL '{url}' has been accessed {instance.url_count[url]} times.")
+            return func(instance, *args, **kwargs)
+        return wrapper
+
+    @count_access
+    @cached(cache)
+    def get_page(self, url: str) -> str:
+        """function uses the requests module to get the HTML content of a URL"""
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
