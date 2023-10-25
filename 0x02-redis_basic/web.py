de190ae57a5d2e21336ef9eea6ade0fd31454533
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-25 13:49:38 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -21,7 +21,6 @@ def wrapper(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return wrapped
